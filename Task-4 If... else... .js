df3d59a1885d801144f11828a3d8848316079dc4
Yashesh1195 <155947815+Yashesh1195@users.noreply.github.com>
2024-05-31 20:26:30 +0530

    Add files via upload

---
+++ Task-4 If... else... .js
@@ -0,0 +1,18 @@
+let d = new Date().getHours();
+        if(d<12){
+            document.getElementById("img").style.backgroundImage = "url('GM.jpeg')";
+            document.getElementById("img").style.backgroundRepeat = "no-repeat";
+        }
+        else if(d>=12 && d<=15){
+            document.getElementById("img").style.backgroundImage = "url('GA.jpeg')";
+            document.getElementById("img").style.backgroundRepeat = "no-repeat";
+        }
+        else if(d>15 && d<=20){
+
+            document.getElementById("img").style.backgroundImage = "url('GE.jpg')";
+            document.getElementById("img").style.backgroundRepeat = "no-repeat";
+        }
+        else{
+            document.getElementById("img").style.backgroundImage = "url('GN.jpg')";
+            document.getElementById("img").style.backgroundRepeat = "no-repeat";
+        }
